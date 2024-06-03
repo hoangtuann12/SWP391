cf378895e7897b5626cf341938e4e2e35d4cb441
lehoangtuan <tuanhoangle10042001@gmail.com>
2024-06-03 21:06:22 +0700

    Login

--- SWP391/src/main/java/com/example/SWP391/Respository/AccountRepository.java
+++ SWP391/src/main/java/com/example/SWP391/Respository/AccountRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     boolean existsByEmail(String email);
-    Optional<Account> findByEmail(String email);
+    Account findByEmail(String email);
 }
