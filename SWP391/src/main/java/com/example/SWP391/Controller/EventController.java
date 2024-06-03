f5026942d0bc6184f867ed68bbbceaf998470a48
ngthaiduy <ntd.thaiduy@gmail.com>
2024-06-03 21:35:51 +0700

    list event

---
+++ SWP391/src/main/java/com/example/SWP391/Controller/EventController.java
@@ -0,0 +1,22 @@
+package com.example.SWP391.Controller;
+
+import com.example.SWP391.Entity.Event;
+import com.example.SWP391.Service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventController {
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
+}
---
+++ SWP391/src/main/java/com/example/SWP391/Respository/EventRepository.java
@@ -0,0 +1,10 @@
+package com.example.SWP391.Respository;
+
+
+import com.example.SWP391.Entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+
+
+}
